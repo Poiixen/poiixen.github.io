0102b46e71fbb6a79e7ae22e285964efe4bc8491
Jorge Garcia <tgarcia5920@gmail.com>
2024-07-28 23:31:31 -0400

    more changes
    Added:
        Resume page,
        Projects page,
        Contact page,
        Hire me button!
    Nav bar button with links to each page, page transition eases in, rather than blinks to page

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,17 +6,26 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-  <header className="py-8 xl:py-12 text-white bg-pink-50/20">
-    <div className="comtainer mx-auto">
-      {}
-      <Link href="/">
-        <h1 className="text-4xl font-semibold">
-          Tito <span className="text-accent">.</span>
-        </h1>
-      </Link>
-      {}
-      <Nav/>
-    </div>
+    <header className="py-8 xl:py-12 text-white">
+      {/* logo */}
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Tito<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+      {/* Desktop nav & hire me */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+            <Link href="/contact">
+              <Button>Hire me</Button>
+            </Link>
+        </div>
+
+      {/* Mobile nav */}
+        <div className="xl:hidden">mobile nav</div>
+      </div>
   </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   prefix: "",
   theme: {
@@ -14,16 +14,15 @@ module.exports = {
       padding: "15px",    
     },
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1200px',
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
     fontFamily: {
       primary: "var(--font-jetbrains-mono)",
     },
     extend: {
-
       colors: {
         primary: "#1c1c22",
         accent: {
@@ -31,7 +30,6 @@ module.exports = {
           dark: "#00e187",
         },
       },
-
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
