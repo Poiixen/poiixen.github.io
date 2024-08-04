e2d982b150d8e4306e001938a6cc51b4ef188930
Jorge Garcia <tgarcia5920@gmail.com>
2024-08-03 23:08:15 -0400

    Photo updated
    blended in to background, change pic to transparent background

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Engineer</span>
             <h1 className="h1">
               Hello I'm <br /> <span className="text-accent">Jorge Garcia</span>
@@ -29,7 +29,6 @@ const Home = () => {
                 size="lg" 
                 className="uppercase flex items-center gap-2"
               >
-            
                 <span>Download Resume</span>
                 <FiDownload className="text-xl" />
               </Button>
@@ -44,7 +43,7 @@ const Home = () => {
             </div>
           </div>
           {/* image */}
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 const Photo = () => {
-  return (
-    <div>
-    Photo
-    </div>
-  )
-}
+  return <div className="w-full h-full relative">
+    <motion.div 
+    initial={{ opacity:0 }} 
+    animate={{
+      opacity: 1, 
+      transition: { delay: .3, duation: 0.4, ease: "easeIn" },
+      }}
+    >
+      <motion.div
+      initial={{ opacity:0 }} 
+      animate={{
+        opacity: 1, 
+        transition: { delay: .7, duation: 0.4, ease: "easeInOut" },
+      }}      
+      className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+      >
+        <Image 
+          src="/assets/linkedinprofilepic.png" 
+          priority 
+          quality={100} 
+          fill 
+          alt="" 
+          className="object-contain"
+        />
+      </motion.div>
+    </motion.div>
+  </div>;
+};
 
 export default Photo;
