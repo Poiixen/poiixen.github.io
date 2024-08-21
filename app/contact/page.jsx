a3c844664d0f0b25fcbcdfa68437d8c218256817
Jorge Garcia <tgarcia5920@gmail.com>
2024-08-20 23:52:26 -0400

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+"use client"; 
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import { 
+  Select,
+  SelectContent, 
+  SelectGroup, 
+  SelectItem, 
+  SelectLabel, 
+  SelectTrigger, 
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "+786 212 5112",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "tgarcia5920@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Location",
+    description: "Gainesville, FL",
+  } 
+]
+
+import { motion } from "framer-motion";
 
 const Contact = () => {
-  return (
-    <div>
-    contact page
-    </div>
-  )
+  return <motion.section initial>contact page</motion.section>
 };
 
 export default Contact;
