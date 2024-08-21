6937bddd50f36b74a3fc3506786bf81b710e9b7f
Jorge Garcia <tgarcia5920@gmail.com>
2024-08-21 06:38:42 -0400

    Contact form completed
    button functionality not implemented yet

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -49,11 +49,11 @@ const Contact = () => {
     <div className="container mx-auto">
       <div className="flex flex-col xl:flex-row gap-[30px]">
         { /* Form */ }
-        <div className="xl:h-[54%] order-2 xl:order-none">
+        <div className="xl:w-[64%] order-2 xl:order-none">
           <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
             <h3 className="text-4xl text-accent">Get in touch!</h3>
             <p className="text-white/60">
-              Fill out the form below and I will get back to you as soon as possible.
+              Fill out the form below and I will get back to you as soon as possible. box wraps to text 
             </p>
             { /* input */ }
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -69,19 +69,37 @@ const Contact = () => {
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
-                  <SelectLabel>Reason for contacting</SelectLabel>
                   <SelectItem value="est">General Inquiry</SelectItem>
                   <SelectItem value="cst">Feedback</SelectItem>
                   <SelectItem value="mst">Collaboration</SelectItem>
-                  <SelectItem>Other</SelectItem>
+                  <SelectItem value="oth">Other</SelectItem>
                 </SelectGroup>
               </SelectContent>
             </Select>
+            { /* textarea */ }
+            <Textarea 
+              className="h-[150px]" 
+              placeholder="Message" 
+            />
+            { /* button */ }
+            <Button size="md" className="max-w-40">Send Message</Button>
           </form>
         </div>
         { /* Info */ }
         <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-          info
+          <ul className="flex flex-col gap-10">
+            {info.map((item, index) => {
+              return <li key={index} className="flex items-center gap-6">
+                <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                  <div className="text-[28px]">{item.icon}</div>
+                </div>
+                <div className="flex-1">
+                  <p className="text-white/60">{item.title}</p>
+                  <h3 className="text-xl">{item.description}</h3>
+                </div>
+              </li>
+            })}
+          </ul>
         </div>
       </div>
     </div>    
